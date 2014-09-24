ddfcbdbcd9b9ba1bbaa4bb5cb6dba33501afe71f
Archagon <archagon@gmail.com>
2014-09-24 03:06:16 +0200

    Project file reorg. Some new stub files.

---
+++ Keyboard/Catboard.swift
@@ -0,0 +1,16 @@
+//
+//  Catboard.swift
+//  TransliteratingKeyboard
+//
+//  Created by Alexei Baboulevitch on 9/24/14.
+//  Copyright (c) 2014 Apple. All rights reserved.
+//
+
+import Foundation
+
+// demo delegate of keyboard
+class Catboard {
+    func createKeyboard() -> Keyboard? {
+        return nil
+    }
+}
---
+++ Keyboard/TastyKeyboardProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  TastyKeyboardProtocol.swift
+//  TransliteratingKeyboard
+//
+//  Created by Alexei Baboulevitch on 9/24/14.
+//  Copyright (c) 2014 Apple. All rights reserved.
+//
+
+protocol TastyKeyboardProtocol {
+    func keyPressed()
+    func keyReleased()
+}
