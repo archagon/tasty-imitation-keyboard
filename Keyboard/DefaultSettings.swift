a1af8faf0397aba683ace3f37aa308b10d5816ac
Archagon <archagon@gmail.com>
2014-11-04 03:29:53 +0100

    More settings improvements.

--- Keyboard/DefaultSettings.swift
+++ Keyboard/DefaultSettings.swift
@@ -8,23 +8,13 @@
 
 import UIKit
 
-// TODO: move this somewhere else and localize
-let settings = [
-    (kAutoCapitalization, "Auto-Capitalization"),
-    (kPeriodShortcut,  "“.” Shortcut"),
-    (kKeyboardClicks, "Keyboard Clicks")
-]
-
-let notes = [
-    kKeyboardClicks: "Please note that keyboard clicks will work only if “Allow Full Access” is enabled in the keyboard settings. Unfortunately, this is a limitation of the operating system. Rest assured that absoutely none of your keystrokes will be recorded or sent over the network with this setting enabled — it is strictly for sound playback and nothing else."
-]
-
 class DefaultSettings: ExtraView, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var tableView: UITableView?
     @IBOutlet var effectsView: UIVisualEffectView?
     @IBOutlet var backButton: UIButton?
     @IBOutlet var settingsLabel: UILabel?
+    @IBOutlet var pixelLine: UIView?
     
     override var darkMode: Bool {
         didSet {
@@ -36,6 +26,18 @@ class DefaultSettings: ExtraView, UITableViewDataSource, UITableViewDelegate {
     let cellBackgroundColorLight = UIColor.whiteColor().colorWithAlphaComponent(CGFloat(1))
     let cellLabelColorDark = UIColor.whiteColor()
     let cellLabelColorLight = UIColor.blackColor()
+    let cellLongLabelColorDark = UIColor.lightGrayColor()
+    let cellLongLabelColorLight = UIColor.grayColor()
+    
+    // TODO: move this somewhere else and localize
+    let settings = [
+        (kAutoCapitalization, "Auto-Capitalization"),
+        (kPeriodShortcut,  "“.” Shortcut"),
+        (kKeyboardClicks, "Keyboard Clicks")
+    ]
+    let notes = [
+        kKeyboardClicks: "Please note that keyboard clicks will work only if “Allow Full Access” is enabled in the keyboard settings. Unfortunately, this is a limitation of the operating system. Rest assured that absoutely none of your keystrokes will be recorded or sent over the network with this setting enabled — it is strictly for sound playback and nothing else."
+    ]
     
     required init(globalColors: GlobalColors.Type?, darkMode: Bool, solidColorMode: Bool) {
         fatalError("this class requires a nib")
@@ -43,15 +45,18 @@ class DefaultSettings: ExtraView, UITableViewDataSource, UITableViewDelegate {
 
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        self.tableView?.registerClass(DefaultSettingsTableViewCell.self, forCellReuseIdentifier: "cell")
+        self.tableView?.estimatedRowHeight = 44;
+        self.tableView?.rowHeight = UITableViewAutomaticDimension;
+
         self.updateAppearance(self.darkMode)
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return settings.count
-    }
-    
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 44
     }
     
     func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
@@ -66,45 +71,32 @@ class DefaultSettings: ExtraView, UITableViewDataSource, UITableViewDelegate {
         return "General Settings"
     }
     
-    // TODO: I couldn't add a prototype cell to the table view in the nib for some reason
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell = UITableViewCell()
-        var sw = UISwitch()
-        var label = UILabel()
-        sw.setTranslatesAutoresizingMaskIntoConstraints(false)
-        label.setTranslatesAutoresizingMaskIntoConstraints(false)
-        sw.tag = 1
-        label.tag = 2
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell") as DefaultSettingsTableViewCell
         
-        sw.on = NSUserDefaults.standardUserDefaults().boolForKey(settings[indexPath.row].0)
-        label.text = settings[indexPath.row].1
-        label.sizeToFit()
+        let key = settings[indexPath.row].0
         
-        sw.addTarget(self, action: Selector("toggleSetting:"), forControlEvents: UIControlEvents.ValueChanged)
+        if cell.sw.allTargets().count == 0 {
+            cell.sw.addTarget(self, action: Selector("toggleSetting:"), forControlEvents: UIControlEvents.ValueChanged)
+        }
         
-        cell.addSubview(sw)
-        cell.addSubview(label)
-        
-        let left = NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: cell, attribute: NSLayoutAttribute.LeftMargin, multiplier: 1, constant: 0)
-        let labelCenterY = NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: cell, attribute: NSLayoutAttribute.CenterY, multiplier: 1, constant: 0)
-        let right = NSLayoutConstraint(item: sw, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: cell, attribute: NSLayoutAttribute.RightMargin, multiplier: 1, constant: 0)
-        let swCenterY = NSLayoutConstraint(item: sw, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: cell, attribute: NSLayoutAttribute.CenterY, multiplier: 1, constant: 0)
-        
-        cell.addConstraint(left)
-        cell.addConstraint(right)
-        cell.addConstraint(labelCenterY)
-        cell.addConstraint(swCenterY)
+        cell.sw.on = NSUserDefaults.standardUserDefaults().boolForKey(key)
+        cell.label.text = settings[indexPath.row].1
+        cell.longLabel.text = notes[key]
         
         cell.backgroundColor = (self.darkMode ? cellBackgroundColorDark : cellBackgroundColorLight)
-        label.textColor = (self.darkMode ? cellLabelColorDark : cellLabelColorLight)
-
+        cell.label.textColor = (self.darkMode ? cellLabelColorDark : cellLabelColorLight)
+        cell.longLabel.textColor = (self.darkMode ? cellLongLabelColorDark : cellLongLabelColorLight)
+        
         return cell
     }
     
     func updateAppearance(dark: Bool) {
         if dark {
             self.effectsView?.effect
-            self.backButton?.setTitleColor(UIColor(red: 135/CGFloat(255), green: 206/CGFloat(255), blue: 250/CGFloat(255), alpha: 1), forState: UIControlState.Normal)
+            let blueColor = UIColor(red: 135/CGFloat(255), green: 206/CGFloat(255), blue: 250/CGFloat(255), alpha: 1)
+            self.pixelLine?.backgroundColor = blueColor.colorWithAlphaComponent(CGFloat(0.5))
+            self.backButton?.setTitleColor(blueColor, forState: UIControlState.Normal)
             self.settingsLabel?.textColor = UIColor.whiteColor()
             
             if let visibleCells = self.tableView?.visibleCells() {
@@ -113,12 +105,16 @@ class DefaultSettings: ExtraView, UITableViewDataSource, UITableViewDelegate {
                         cell.backgroundColor = cellBackgroundColorDark
                         var label = cell.viewWithTag(2) as? UILabel
                         label?.textColor = cellLabelColorDark
+                        var longLabel = cell.viewWithTag(3) as? UITextView
+                        longLabel?.textColor = cellLongLabelColorDark
                     }
                 }
             }
         }
         else {
-            self.backButton?.setTitleColor(UIColor(red: 0/CGFloat(255), green: 122/CGFloat(255), blue: 255/CGFloat(255), alpha: 1), forState: UIControlState.Normal)
+            let blueColor = UIColor(red: 0/CGFloat(255), green: 122/CGFloat(255), blue: 255/CGFloat(255), alpha: 1)
+            self.pixelLine?.backgroundColor = blueColor.colorWithAlphaComponent(CGFloat(0.5))
+            self.backButton?.setTitleColor(blueColor, forState: UIControlState.Normal)
             self.settingsLabel?.textColor = UIColor.grayColor()
             
             if let visibleCells = self.tableView?.visibleCells() {
@@ -127,6 +123,8 @@ class DefaultSettings: ExtraView, UITableViewDataSource, UITableViewDelegate {
                         cell.backgroundColor = cellBackgroundColorLight
                         var label = cell.viewWithTag(2) as? UILabel
                         label?.textColor = cellLabelColorLight
+                        var longLabel = cell.viewWithTag(3) as? UITextView
+                        longLabel?.textColor = cellLongLabelColorLight
                     }
                 }
             }
@@ -139,6 +137,99 @@ class DefaultSettings: ExtraView, UITableViewDataSource, UITableViewDelegate {
                 let key = settings[indexPath.row].0
                 NSUserDefaults.standardUserDefaults().setBool(sender.on, forKey: key)
             }
+        }
+    }
+}
+
+class DefaultSettingsTableViewCell: UITableViewCell {
+    
+    var sw: UISwitch
+    var label: UILabel
+    var longLabel: UITextView
+    var constraintsSetForLongLabel: Bool
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        self.sw = UISwitch()
+        self.label = UILabel()
+        self.longLabel = UITextView()
+        
+        self.constraintsSetForLongLabel = false
+        
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.sw.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.label.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.longLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        self.longLabel.text = nil
+        self.longLabel.scrollEnabled = false
+        self.longLabel.selectable = false
+        self.longLabel.backgroundColor = UIColor.clearColor()
+        
+        self.sw.tag = 1
+        self.label.tag = 2
+        self.longLabel.tag = 3
+
+        self.addSubview(self.sw)
+        self.addSubview(self.label)
+        self.addSubview(self.longLabel)
+        
+        self.addConstraints()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addConstraints() {
+        let hasLongText = self.longLabel.text != nil && !self.longLabel.text.isEmpty
+        if hasLongText {
+            let switchSide = NSLayoutConstraint(item: sw, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.RightMargin, multiplier: 1, constant: 0)
+            let switchTop = NSLayoutConstraint(item: sw, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.TopMargin, multiplier: 1, constant: 0)
+            let labelSide = NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.LeftMargin, multiplier: 1, constant: 0)
+            let labelCenter = NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: sw, attribute: NSLayoutAttribute.CenterY, multiplier: 1, constant: 0)
+            
+            self.addConstraint(switchSide)
+            self.addConstraint(switchTop)
+            self.addConstraint(labelSide)
+            self.addConstraint(labelCenter)
+            
+            let left = NSLayoutConstraint(item: longLabel, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.LeftMargin, multiplier: 1, constant: 0)
+            let right = NSLayoutConstraint(item: longLabel, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.RightMargin, multiplier: 1, constant: 0)
+            let top = NSLayoutConstraint(item: longLabel, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: sw, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: 8)
+            let bottom = NSLayoutConstraint(item: longLabel, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.BottomMargin, multiplier: 1, constant: 0)
+            
+            self.addConstraint(left)
+            self.addConstraint(right)
+            self.addConstraint(top)
+            self.addConstraint(bottom)
+            
+            self.constraintsSetForLongLabel = true
+        }
+        else {
+            let switchSide = NSLayoutConstraint(item: sw, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.RightMargin, multiplier: 1, constant: 0)
+            let switchTop = NSLayoutConstraint(item: sw, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.TopMargin, multiplier: 1, constant: 0)
+            let switchBottom = NSLayoutConstraint(item: sw, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.BottomMargin, multiplier: 1, constant: 0)
+            let labelSide = NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.LeftMargin, multiplier: 1, constant: 0)
+            let labelCenter = NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: sw, attribute: NSLayoutAttribute.CenterY, multiplier: 1, constant: 0)
+            
+            self.addConstraint(switchSide)
+            self.addConstraint(switchTop)
+            self.addConstraint(switchBottom)
+            self.addConstraint(labelSide)
+            self.addConstraint(labelCenter)
+            
+            self.constraintsSetForLongLabel = false
+        }
+    }
+    
+    override func updateConstraints() {
+        super.updateConstraints()
+        
+        let hasLongText = self.longLabel.text != nil && !self.longLabel.text.isEmpty
+        if hasLongText != self.constraintsSetForLongLabel {
+            self.removeConstraints(self.constraints())
+            self.addConstraints()
         }
     }
 }
