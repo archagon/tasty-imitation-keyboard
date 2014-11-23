3a86a989cec065b27157964ab1e5985255bf0aa2
Archagon <archagon@gmail.com>
2014-11-23 19:15:27 +0100

    Fixed keyboard key layout size check.

--- Keyboard/KeyboardKey.swift
+++ Keyboard/KeyboardKey.swift
@@ -177,7 +177,7 @@ class KeyboardKey: UIControl {
         if self.bounds.width == 0 || self.bounds.height == 0 {
             return
         }
-        if oldBounds != nil && CGRectEqualToRect(boundingBox, oldBounds!) {
+        if oldBounds != nil && CGSizeEqualToSize(boundingBox.size, oldBounds!.size) {
             return
         }
         oldBounds = boundingBox
